b00c80801e057902218e9916161bd5de988bb94a
Erick Sebadelhe <ericksebadelhe@hotmail.com>
2021-04-04 11:38:38 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => response.json({ message: "Hello world!" }));
+
+app.listen(3333, () => {
+  console.log("Server running on port 3333!");
+});
